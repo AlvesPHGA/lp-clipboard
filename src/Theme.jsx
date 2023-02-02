5f82cf5e6de72eb1164d2f3f817b8e36e83c89b3
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-02 15:37:08 -0300

    add a doc theme for project

---
+++ src/Theme.jsx
@@ -0,0 +1,15 @@
+export const theme = {
+   colors: {
+      strong_cyan_prim: 'hsl(171, 66%, 44%)',
+      light_blue_prim: 'hsl(233, 100%, 69%)',
+      dark_grayish_blue_neu: 'hsl(210, 10%, 33%)',
+      grayish_blue_neu: 'hsl(201, 11%, 66%)',
+   },
+
+   font_size: {
+      sub_title: '1.20rem',
+      title_sec: '1.24rem',
+      title: '2rem',
+      text: '1.125rem',
+   },
+};
