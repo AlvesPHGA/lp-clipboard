18dab9086bab434154f6fc279679e871e5c05433
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-02 18:09:36 -0300

    updated App doc

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
+import Subheader from './sections/subheader';
+
 function App() {
    return (
       <>
-         <h1>hello</h1>
-         <h2>hello</h2>
-         <h3>hello</h3>
-         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-            facere nobis sapiente. Minima, quo necessitatibus in sed eos facilis
-            aperiam omnis veniam, soluta sit quaerat laborum quisquam dolorum,
-            autem iure.
-         </p>
+         <Subheader />
       </>
    );
 }
