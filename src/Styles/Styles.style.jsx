70ae36a8b76b1ffb02db39b26e0dac00f681d48b
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-02 16:40:21 -0300

    updated styles.style doc, was created a new style class with name BoxBtns

--- src/Styles/Styles.style.jsx
+++ src/Styles/Styles.style.jsx
@@ -5,3 +5,9 @@ export const Wrap__1200 = styled.div`
    position: relative;
    width: 1200px;
 `;
+
+export const BoxBtns = styled.div`
+   margin: 0 auto;
+   display: flex;
+   gap: 1.25rem;
+`;
