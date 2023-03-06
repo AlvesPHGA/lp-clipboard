194bfe958d16200aff3d21cc5a9962794aad61e9
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-06 16:19:39 -0300

    created a Workflow section and Workflow style

---
+++ src/sections/Workflow/Workflow.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Wrap__1200 } from '../../Styles/Styles.style';
+import { WorkflowStyle } from './Workflow.style';
+
+const Workflow = () => {
+   return (
+      <WorkflowStyle>
+         <Wrap__1200>
+            <h2>Supercharge your workflow</h2>
+            <p>We've got the tools to boost your productivity.</p>
+         </Wrap__1200>
+      </WorkflowStyle>
+   );
+};
+
+export default Workflow;
