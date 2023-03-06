591464d0b3f7d375dcac846181afa223d355f30e
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-06 16:01:02 -0300

    created a section and style doc with name Access section and Access style

---
+++ src/sections/Access/Access.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Wrap__1200 } from '../../Styles/Styles.style';
+import { AccessStyle } from './Access.style';
+
+const Access = () => {
+   return (
+      <AccessStyle>
+         <Wrap__1200>
+            <h2>Access Clipboard Anywhere</h2>
+            <p>
+               Whether you're on the go, or at your computer, you can access all
+               your Clipboard snippets in a few simple clicks.'
+            </p>
+         </Wrap__1200>
+      </AccessStyle>
+   );
+};
+
+export default Access;
