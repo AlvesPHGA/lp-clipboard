d58d1e842d244cbaffadfaff1dd729ed012ec1b6
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-06 14:40:08 -0300

    add box-shadow in buttons in history section

--- src/components/CTA/CTA.style.jsx
+++ src/components/CTA/CTA.style.jsx
@@ -1,17 +1,23 @@
 import styled from 'styled-components';
 
 export const CTAStyle = styled.a`
-   display: block;
-   width: fit-content;
-   padding: 1rem 1.5rem;
-   cursor: pointer;
-   border-radius: 1.5rem;
-   font-size: 1.25rem;
-   font-weight: 500;
-   color: white;
-
    background: ${(props) =>
       props['data-cta'] === 'green'
          ? props.theme.colors.strong_cyan_prim
          : props.theme.colors.light_blue_prim};
+
+   box-shadow: 0px 2px 0px 1px
+      ${(props) =>
+         props['data-cta'] === 'green'
+            ? 'hsl(171, 66%, 30%)'
+            : 'hsl(233, 100%, 49%)'};
+
+   border-radius: 1.5rem;
+   color: white;
+   cursor: pointer;
+   display: block;
+   font-size: 1.25rem;
+   font-weight: 500;
+   padding: 1rem 1.5rem;
+   width: fit-content;
 `;
