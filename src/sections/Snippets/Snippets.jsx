4fc0050578be7fab522a43b4fe60b602e33d9bc2
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-06 15:54:18 -0300

    add a section in section Snippets

--- src/sections/Snippets/Snippets.jsx
+++ src/sections/Snippets/Snippets.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Wrap__1200 } from '../../Styles/Styles.style';
-import { SnippetsStyle } from './Snippets.style';
+import {
+   BoxFunctionalities,
+   BoxImg,
+   Functionalities,
+   Functionality,
+   SnippetsStyle,
+} from './Snippets.style';
 
 const Snippets = () => {
    return (
@@ -13,6 +19,31 @@ const Snippets = () => {
                devices. Our Mac and iOS apps will help you organize everything.
             </p>
          </Wrap__1200>
+         <Functionalities>
+            <BoxImg></BoxImg>
+            <BoxFunctionalities>
+               <Functionality>
+                  <h3>Quick Search</h3>
+                  <p>
+                     Easily search your snippets by content, category, web
+                     address, application, and more.
+                  </p>
+               </Functionality>
+               <Functionality>
+                  <h3>iCloud Sync</h3>
+                  <p>
+                     Instantly saves and syncs snippets across all your devices.
+                  </p>
+               </Functionality>
+               <Functionality>
+                  <h3>Complete History</h3>
+                  <p>
+                     Retrieve any snippets from the first moment you started
+                     using the app.
+                  </p>
+               </Functionality>
+            </BoxFunctionalities>
+         </Functionalities>
       </SnippetsStyle>
    );
 };
--- src/sections/Snippets/Snippets.style.jsx
+++ src/sections/Snippets/Snippets.style.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import BackComputer from '../../assets/image-computer.png';
+
 export const SnippetsStyle = styled.section`
    text-align: center;
    padding: 5rem 0;
@@ -8,5 +10,42 @@ export const SnippetsStyle = styled.section`
       width: 44rem;
       line-height: 1.875rem;
       margin: 1.25rem auto;
+   }
+`;
+
+export const Functionalities = styled.section`
+   padding-top: 2.5rem;
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+   align-items: center;
+`;
+
+export const BoxImg = styled.div`
+   width: 90%;
+   height: 70vh;
+   background: url(${BackComputer}) no-repeat;
+   background-position: -50px center;
+   background-size: contain;
+`;
+
+export const BoxFunctionalities = styled.div`
+   padding-left: 100px;
+   display: flex;
+   flex-direction: column;
+   gap: 2rem;
+`;
+
+export const Functionality = styled.div`
+   text-align: left;
+
+   & h3 {
+      margin-bottom: 0.75rem;
+   }
+
+   & p {
+      padding: 0;
+      width: 412px;
+      text-align: left;
+      margin: 0;
    }
 `;
