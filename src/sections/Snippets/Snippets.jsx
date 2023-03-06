e894e82d1895e866065b973762824480a13b3d55
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-06 14:55:07 -0300

    created a style doc to Snippets section

--- src/sections/Snippets/Snippets.jsx
+++ src/sections/Snippets/Snippets.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Wrap__1200 } from '../../Styles/Styles.style';
+import { SnippetsStyle } from './Snippets.style';
 
 const Snippets = () => {
    return (
-      <Wrap__1200>
-         <h2>Keep track of your snippets</h2>
-         <p>
-            Clipboard instantly stores any item you copy in the cloud, meaning
-            you can access your snippets immediately on all your devices. Our
-            Mac and iOS apps will help you organize everything.
-         </p>
-      </Wrap__1200>
+      <SnippetsStyle>
+         <Wrap__1200>
+            <h2>Keep track of your snippets</h2>
+            <p>
+               Clipboard instantly stores any item you copy in the cloud,
+               meaning you can access your snippets immediately on all your
+               devices. Our Mac and iOS apps will help you organize everything.
+            </p>
+         </Wrap__1200>
+      </SnippetsStyle>
    );
 };
 
---
+++ src/sections/Snippets/Snippets.style.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SnippetsStyle = styled.section`
+   text-align: center;
+   padding: 5rem 0;
+
+   & p {
+      width: 44rem;
+      line-height: 1.875rem;
+      margin: 1.25rem auto;
+   }
+`;
