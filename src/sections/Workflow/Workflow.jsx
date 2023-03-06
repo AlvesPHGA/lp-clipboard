f8b46459c7bfc41b04b2c3200167669894132330
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-06 20:09:58 -0300

    updated Workflow section and Workflow style

--- src/sections/Workflow/Workflow.jsx
+++ src/sections/Workflow/Workflow.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Wrap__1200 } from '../../Styles/Styles.style';
-import { WorkflowStyle } from './Workflow.style';
+import { BoxWork, Work, WorkflowStyle } from './Workflow.style';
 
 const Workflow = () => {
    return (
@@ -8,6 +8,10 @@ const Workflow = () => {
          <Wrap__1200>
             <h2>Supercharge your workflow</h2>
             <p>We've got the tools to boost your productivity.</p>
+
+            <BoxWork>
+               <Work></Work>
+            </BoxWork>
          </Wrap__1200>
       </WorkflowStyle>
    );
--- src/sections/Workflow/Workflow.style.jsx
+++ src/sections/Workflow/Workflow.style.jsx
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
 export const WorkflowStyle = styled.section``;
+
+export const BoxWork = styled.section``;
+
+export const Work = styled.div``;
