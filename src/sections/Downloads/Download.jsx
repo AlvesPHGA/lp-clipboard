7ef1c726bfa083a51c37e905c981913f90a4490a
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-09 16:35:34 -0300

    created a new section with name Download and created a style doc to Download section

---
+++ src/sections/Downloads/Download.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Wrap__1200 } from '../../Styles/Styles.style';
+import { DownloadStyle } from './Download.style';
+
+const Download = () => {
+   return (
+      <DownloadStyle>
+         <Wrap__1200>
+            <h2>Clipboard for iOS and Mac OS</h2>
+            <p>
+               Available for free on the App Store. Download for Mac or iOS,
+               sync with iCloud and you're ready to start adding to your
+               clipboard.
+            </p>
+         </Wrap__1200>
+      </DownloadStyle>
+   );
+};
+
+export default Download;
---
+++ src/sections/Downloads/Download.style.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const DownloadStyle = styled.section`
+   padding: 5rem 0;
+`;
