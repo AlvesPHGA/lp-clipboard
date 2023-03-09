fdcd38c6602858c7b5e4de04276663922650e135
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-09 16:16:20 -0300

    created a new section with name Companies and a style doc to Companies

---
+++ src/sections/Companies/Companies.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Wrap__1200 } from '../../Styles/Styles.style';
+import { CompaniesStyle, Company } from './Companies.style';
+
+const Companies = () => {
+   return (
+      <CompaniesStyle>
+         <Wrap__1200>
+            <Company></Company>
+            <Company></Company>
+            <Company></Company>
+            <Company></Company>
+         </Wrap__1200>
+      </CompaniesStyle>
+   );
+};
+
+export default Companies;
---
+++ src/sections/Companies/Companies.style.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const CompaniesStyle = styled.section``;
+
+export const Company = styled.div``;
