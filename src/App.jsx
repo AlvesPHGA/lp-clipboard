4f3f749f8a76dca60e66b9770f6dce84e0cc892e
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-20 17:15:23 -0300

    add a useEffect in project

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,26 @@ import History from './sections/History/History';
 import Snippets from './sections/Snippets/Snippets';
 import Header from './sections/Header/Header';
 import Workflow from './sections/Workflow/Workflow';
+import React from 'react';
 
 function App() {
+   React.useEffect(() => {
+      const elements = document.querySelectorAll('section');
+      const heightWin = window.innerHeight;
+
+      window.addEventListener('scroll', () => {
+         elements.forEach((element) => {
+            const el = element.getBoundingClientRect().y;
+            if (el < heightWin * 0.8) {
+               element.classList.add('active');
+            }
+            if (el < heightWin * 0.1 || el > heightWin * 0.8) {
+               element.classList.remove('active');
+            }
+         });
+      });
+   }, []);
+
    return (
       <>
          <Header />
