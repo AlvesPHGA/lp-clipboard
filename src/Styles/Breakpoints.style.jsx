3eb39dbca6c02c6ea09c8389dfe2a6944c1748c3
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-24 11:21:52 -0300

    updated style breakpoint

--- src/Styles/Breakpoints.style.jsx
+++ src/Styles/Breakpoints.style.jsx
@@ -1,19 +1,19 @@
 const size = {
-   mobileS: '320px',
-   mobileM: '375px',
-   mobileL: '425px',
-   tablet: '768px',
-   laptop: '1024px',
-   laptopL: '1440px',
-   desktop: '2560px',
+   mobileS: [320],
+   mobileM: [375, 424],
+   mobileL: [425, 767],
+   tablet: [768, 1023],
+   laptop: [1024, 1439],
+   laptopL: [1440, 2559],
+   desktop: [2560],
 };
 
 export const device = {
-   mobileS: `(max-width: ${size.mobileS})`,
-   mobileM: `(min-width: ${size.mobileM})`,
-   mobileL: `(min-width: ${size.mobileL})`,
-   tablet: `(min-width: ${size.tablet})`,
-   laptop: `(min-width: ${size.laptop})`,
-   laptopL: `(min-width: ${size.laptopL})`,
-   desktop: `(min-width: ${size.desktop})`,
+   mobileS: `(max-width: ${size.mobileS[0]}px)`,
+   mobileM: `(min-width: ${size.mobileM[0]}px) and (max-width: ${size.mobileM[1]}px)`,
+   mobileL: `(min-width: ${size.mobileL[0]}px) and (max-width: ${size.mobileL[1]}px)`,
+   tablet: `(min-width: ${size.tablet[0]}px) and (max-width: ${size.tablet[1]}px)`,
+   laptop: `(min-width: ${size.laptop[0]}px) and (max-width: ${size.laptop[1]}px)`,
+   laptopL: `(min-width: ${size.laptopL[0]}px) and (max-width: ${size.laptopL[1]}px)`,
+   desktop: `(min-width: ${size.desktop[0]}px)`,
 };
